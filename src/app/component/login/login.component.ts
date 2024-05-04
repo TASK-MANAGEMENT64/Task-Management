6f4e222f133060319568c156d0632e8364205725
Lorenzo Navarro <u201713141@upc.edu.pe>
2024-05-04 15:32:08 -0500

    (fix)primeicons dependecy deleted

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component } from '@angular/core';
-import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
-import {Router, RouterLink} from '@angular/router';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 import { MessageService } from 'primeng/api';
-import {CardModule} from "primeng/card";
-import {InputTextModule} from "primeng/inputtext";
-import {ButtonModule} from "primeng/button";
-import {NgIf} from "@angular/common";
-import "primeng/resources/themes/saga-blue/theme.css";
-import "primeng/resources/primeng.min.css";
-import "primeicons/primeicons.css";
-
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { NgIf } from '@angular/common';
+// import 'primeng/resources/themes/saga-blue/theme.css';
+// import 'primeng/resources/primeng.min.css';
+// import "primeicons/primeicons.css";
 
 @Component({
   selector: 'app-login',
@@ -25,23 +29,24 @@ import "primeicons/primeicons.css";
     ReactiveFormsModule,
   ],
   providers: [MessageService],
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
   loginForm = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
-    password: ['', Validators.required]
-  })
+    password: ['', Validators.required],
+  });
 
   constructor(
     private fb: FormBuilder,
     private router: Router,
     private msgService: MessageService
-  ) { }
+  ) {}
 
   get email() {
     return this.loginForm.controls['email'];
   }
-  get password() { return this.loginForm.controls['password']; }
-
+  get password() {
+    return this.loginForm.controls['password'];
+  }
 }
